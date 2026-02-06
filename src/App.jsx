fb1092496058d7fb92bff52fa3111c689916f4b8
Anastasia Walia <anastasiawalia@MacBookPro.home.local>
2026-02-05 16:30:20 -0800

    Initial commit: React app with Three.js particle animation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero";
+import ContentSection from "./components/ContentSection";
+
+function App() {
+  return (
+    <div className="size-full">
+      <Hero />
+      <ContentSection />
+    </div>
+  );
+}
+
+export default App;
